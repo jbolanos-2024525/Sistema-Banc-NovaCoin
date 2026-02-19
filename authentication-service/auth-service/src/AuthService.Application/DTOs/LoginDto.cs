237a9ca1b9968b401818c42a97bcce8788c1ee28
jmendez-2024055 <jmendez-2024055@kinal.edu.gt>
2026-02-18 20:32:34 -0600

    fix: Se edito el archivo LoginDto en la carpeta DTOs en la capa Application

--- authentication-service/auth-service/src/AuthService.Application/DTOs/LoginDto.cs
+++ authentication-service/auth-service/src/AuthService.Application/DTOs/LoginDto.cs
@@ -2,6 +2,6 @@ namespace AuthService.Application.DTOs;
 
 public class LoginDto
 {
-    public string Username { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    public required string Username { get; set; }
+    public required string Password { get; set; }
 }
