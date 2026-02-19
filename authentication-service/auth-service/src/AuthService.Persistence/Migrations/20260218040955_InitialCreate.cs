d719a2335d71819e9c14de6ddb60ec58d857baf2
jbolanos-2024525 <jbolanos-2024525@gmail.com>
2026-02-18 22:28:47 -0600

    Delete(RBK-008): Se elimino la clase InMemoryUserRepository.cs de la carpeta repositories dentro de la capa persistence

---
+++ authentication-service/auth-service/src/AuthService.Persistence/Migrations/20260218040955_InitialCreate.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AuthService.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "clientes",
+                columns: table => new
+                {
+                    id_cliente = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    nombre = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    apellido = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    dpi = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    direccion = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    telefono = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: false),
+                    correo = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_clientes", x => x.id_cliente);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "empleados",
+                columns: table => new
+                {
+                    id_empleado = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    nombre = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    apellido = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    cargo = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    telefono = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: false),
+                    correo = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_empleados", x => x.id_empleado);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "roles",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+                    name = table.Column<string>(type: "character varying(35)", maxLength: 35, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_roles", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "transacciones",
+                columns: table => new
+                {
+                    id_transaccion = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    id_cuenta = table.Column<int>(type: "integer", nullable: false),
+                    id_cuenta_origen = table.Column<int>(type: "integer", nullable: false),
+                    id_cuenta_destino = table.Column<int>(type: "integer", nullable: false),
+                    tipo = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    tipo_transaccion = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    monto = table.Column<decimal>(type: "numeric(12,2)", nullable: false),
+                    fecha = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    fecha_transaccion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    descripcion = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    cuenta_origen = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    cuenta_destino = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_transacciones", x => x.id_transaccion);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    surname = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    username = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    password = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    status = table.Column<bool>(type: "boolean", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "cuentas",
+                columns: table => new
+                {
+                    id_cuenta = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    id_cliente = table.Column<int>(type: "integer", nullable: false),
+                    numero_cuenta = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    tipo_cuenta = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    saldo = table.Column<decimal>(type: "numeric(12,2)", nullable: false),
+                    fecha_apertura = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_DATE"),
+                    activa = table.Column<bool>(type: "boolean", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_cuentas", x => x.id_cuenta);
+                    table.ForeignKey(
+                        name: "fk_cuentas_clientes_id_cliente",
+                        column: x => x.id_cliente,
+                        principalTable: "clientes",
+                        principalColumn: "id_cliente",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "prestamos",
+                columns: table => new
+                {
+                    id_prestamo = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    id_cliente = table.Column<int>(type: "integer", nullable: false),
+                    monto = table.Column<decimal>(type: "numeric(12,2)", nullable: false),
+                    tasa_interes = table.Column<decimal>(type: "numeric(5,2)", nullable: false),
+                    plazo_meses = table.Column<int>(type: "integer", nullable: false),
+                    monto_total = table.Column<decimal>(type: "numeric(12,2)", nullable: false),
+                    monto_pendiente = table.Column<decimal>(type: "numeric(12,2)", nullable: false),
+                    fecha_prestamo = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    fecha_solicitud = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    fecha_finalizacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    estado = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_prestamos", x => x.id_prestamo);
+                    table.ForeignKey(
+                        name: "fk_prestamos_clientes_id_cliente",
+                        column: x => x.id_cliente,
+                        principalTable: "clientes",
+                        principalColumn: "id_cliente",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_emails",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+                    user_id = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+                    email_verified = table.Column<bool>(type: "boolean", nullable: false),
+                    email_verification_token = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    email_verification_token_expiry = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_emails", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_user_emails_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_password_resets",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+                    user_id = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+                    password_reset_token = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    password_reset_token_expiry = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_password_resets", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_user_password_resets_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_profiles",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+                    user_id = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+                    profile_picture = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    phone = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_profiles", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_user_profiles_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_roles",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+                    user_id = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+                    role_id = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_roles", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_user_roles_roles_role_id",
+                        column: x => x.role_id,
+                        principalTable: "roles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_user_roles_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_clientes_dpi",
+                table: "clientes",
+                column: "dpi",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_cuentas_id_cliente",
+                table: "cuentas",
+                column: "id_cliente");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_cuentas_numero_cuenta",
+                table: "cuentas",
+                column: "numero_cuenta",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_prestamos_id_cliente",
+                table: "prestamos",
+                column: "id_cliente");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_emails_user_id",
+                table: "user_emails",
+                column: "user_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_password_resets_user_id",
+                table: "user_password_resets",
+                column: "user_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_profiles_user_id",
+                table: "user_profiles",
+                column: "user_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_roles_role_id",
+                table: "user_roles",
+                column: "role_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_roles_user_id",
+                table: "user_roles",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_email",
+                table: "users",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_username",
+                table: "users",
+                column: "username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "cuentas");
+
+            migrationBuilder.DropTable(
+                name: "empleados");
+
+            migrationBuilder.DropTable(
+                name: "prestamos");
+
+            migrationBuilder.DropTable(
+                name: "transacciones");
+
+            migrationBuilder.DropTable(
+                name: "user_emails");
+
+            migrationBuilder.DropTable(
+                name: "user_password_resets");
+
+            migrationBuilder.DropTable(
+                name: "user_profiles");
+
+            migrationBuilder.DropTable(
+                name: "user_roles");
+
+            migrationBuilder.DropTable(
+                name: "clientes");
+
+            migrationBuilder.DropTable(
+                name: "roles");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
