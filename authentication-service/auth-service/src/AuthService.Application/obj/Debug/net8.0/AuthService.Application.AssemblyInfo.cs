7ac8c1b0ee96a10c834b69d24b734a79904b9a91
jmendez-2024055 <jmendez-2024055@kinal.edu.gt>
2026-02-18 20:30:41 -0600

    fix: Se edito el archivo AuthResponseDto.cs en la carpeta DTOs en la capa Application

--- authentication-service/auth-service/src/AuthService.Application/obj/Debug/net8.0/AuthService.Application.AssemblyInfo.cs
+++ authentication-service/auth-service/src/AuthService.Application/obj/Debug/net8.0/AuthService.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e56b67c1b9c4e64e73c036f66db6c36845462890")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+654c3376b7863e5457775280c800f71d39139a2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
