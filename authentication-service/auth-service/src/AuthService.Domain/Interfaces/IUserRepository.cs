66d885322606ddf53d4e48c58597f661c01165fa
jmendez-2024055 <jmendez-2024055@kinal.edu.gt>
2026-05-05 22:27:07 -0600

    Fix(RBK-021): Se modifico el Servicio de autenticación correción de errores

--- authentication-service/auth-service/src/AuthService.Domain/Interfaces/IUserRepository.cs
+++ authentication-service/auth-service/src/AuthService.Domain/Interfaces/IUserRepository.cs
@@ -4,6 +4,16 @@ namespace AuthService.Domain.Interfaces;
 
 public interface IUserRepository
 {
-    UserRole? GetByUsername(string username);
-    UserRole Add(UserRole user);
+    Task<User> CreateAsync(User user);
+    Task<IEnumerable<User>> GetUsersAsync();
+    Task<User> GetByIdAsync(string id);
+    Task<User?> GetByEmailAsync(string email);
+    Task<User?> GetByUsernameAsync(string username);
+    Task<User?> GetByEmailVerificationTokenAsync(string token);
+    Task<User?> GetByPasswordResetTokenAsync(string token);
+    Task<bool> ExistsByEmailAsync(string email);
+    Task<bool> ExistsByUsernameAsync(string username);
+    Task<User> UpdateAsync(User user);
+    Task<bool> DeleteAsync(string id);
+    Task UpdateUserRoleAsync(string userId, string roleId);
 }
